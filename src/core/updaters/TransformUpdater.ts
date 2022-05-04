4e71f1334010a7c398de8c9a1e6d258130a987f9
ics-kaga <kaga@ics-web.jp>
2022-05-04 10:47:56 +0900

    パフォーマンス最適化
    ・Styleへの参照を修正
    ・フラグの管理方法を変更

--- src/core/updaters/TransformUpdater.ts
+++ src/core/updaters/TransformUpdater.ts
@@ -12,7 +12,7 @@ export class TransformUpdater implements Updater {
     private _target    :HTMLElement;
     private _query     :string|null;
     private _pseudo    :string|null;
-    private _style     :CSSStyleDeclaration|undefined;
+    private _style     :CSSStyleDeclaration;
     private _tweenQuery:string|null;
 
     private _useWillChange:boolean;
@@ -26,20 +26,13 @@ export class TransformUpdater implements Updater {
     private _skewY   :ParamUpdater|null;
     private _rotation:ParamUpdater|null;
 
-    private _updateX       :boolean;
-    private _updateY       :boolean;
-    private _updateScaleX  :boolean;
-    private _updateScaleY  :boolean;
-    private _updateSkewX   :boolean;
-    private _updateSkewY   :boolean;
-    private _updateRotation:boolean;
-
     private _percentX:string|null;
     private _percentY:string|null;
 
     constructor(target:HTMLElement, query:string|null) {
         this._target = target;
         this._query  = query;
+        this._style  = target.style;
         this._matrix = new Matrix();
 
         this._x        = null;
@@ -50,49 +43,39 @@ export class TransformUpdater implements Updater {
         this._skewY    = null;
         this._rotation = null;
 
-        this._updateX        = false;
-        this._updateY        = false;
-        this._updateScaleX   = false;
-        this._updateScaleY   = false;
-        this._updateSkewX    = false;
-        this._updateSkewY    = false;
-        this._updateRotation = false;
-
         this._percentX = null;
         this._percentY = null;
         
         this._pseudo     = this._query ? HTMLUtil.getPseudoQuery(this._query) : null;
         this._tweenQuery = null;
 
-        this._useWillChange = true;
+        this._useWillChange = false;
     }
 
     init(useWillChange:boolean) {
         this._useWillChange = useWillChange;
 
         // Setting style
-        if (!this._style) {
-            if (this._pseudo && this._query) {
-                this._style = HTMLUtil.getAddedStyleByElement(this._target, this._pseudo);
-                if (this._style && HTMLUtil.getComputedStyle(this._target, this._pseudo).display == "inline") {
+        if (this._pseudo && this._query) {
+            const sty = HTMLUtil.getAddedStyleByElement(this._target, this._pseudo);
+            if (sty) {
+                this._style = sty;
+                if (HTMLUtil.getComputedStyle(this._target, this._pseudo).display == "inline") {
                     this._style.setProperty("display", "inline-block");
                 }
-                this._tweenQuery = HTMLUtil.getTweenElementQuery(this._target, this._pseudo);
             }
-            else {
-                this._style = this._target.style;
-            }
+            this._tweenQuery = HTMLUtil.getTweenElementQuery(this._target, this._pseudo);
         }
 
         // Setting chahe
-        const chache:Matrix|undefined = this._getChache();
+        const chache = this._getChache();
         if (chache)
             this._matrix = Matrix.copy(this._matrix, chache);
         else
             this._matrix.setMatrixByCSSTransform(HTMLUtil.getTransformMatrix(this._target, this._pseudo));
 
         // Set will-change
-        if (this._style && this._useWillChange) {
+        if (this._useWillChange) {
             HTMLUtil.addWillChange(this._style, ["transform"]);
         }
         
@@ -101,11 +84,11 @@ export class TransformUpdater implements Updater {
             if (this._pseudo) {
                 const sty = HTMLUtil.getComputedStyle(this._target, this._pseudo);
                 if (this._percentX) {
-                    if (this._style && !this._style.width) this._style.setProperty("width", "auto");
+                    if (!this._style.width) this._style.setProperty("width", "auto");
                     this._x = new ParamUpdater("x", parseFloat(sty.width) * parseFloat(this._percentX) / 100, this._percentX);
                 }
                 if (this._percentY) {
-                    if (this._style && !this._style.height) this._style.setProperty("height", "auto");
+                    if (!this._style.height) this._style.setProperty("height", "auto");
                     this._y = new ParamUpdater("y", parseFloat(sty.height) * parseFloat(this._percentY) / 100, this._percentY);
                 }
             }
@@ -114,14 +97,6 @@ export class TransformUpdater implements Updater {
                 if (this._percentY) this._y = new ParamUpdater("y", this._target.offsetHeight * parseFloat(this._percentY) / 100, this._percentY);
             }
         }
-        
-        this._updateX        = this._x        ? true : false;
-        this._updateY        = this._y        ? true : false;
-        this._updateScaleX   = this._scaleX   ? true : false;
-        this._updateScaleY   = this._scaleY   ? true : false;
-        this._updateSkewX    = this._skewX    ? true : false;
-        this._updateSkewY    = this._skewY    ? true : false;
-        this._updateRotation = this._rotation ? true : false;
 
         this._x       ?.init(this._matrix.x);
         this._y       ?.init(this._matrix.y);
@@ -133,7 +108,8 @@ export class TransformUpdater implements Updater {
     }
 
     addProp(key:string, value:number, option:string|null = null) {
-        let updater;
+        let updater:ParamUpdater;
+
         if (option) {
             this._matrix.setMatrixByCSSTransform(HTMLUtil.getTransformMatrix(this._target, this._pseudo));
             updater = new ParamUpdater(key, this._matrix.getProp(key), value);
@@ -149,6 +125,7 @@ export class TransformUpdater implements Updater {
         else {
             updater = new ParamUpdater(key, value, value); 
         }
+
         switch (key) {
             case "x"       : this._x        = updater; break;
             case "y"       : this._y        = updater; break;
@@ -185,20 +162,20 @@ export class TransformUpdater implements Updater {
     }
 
     update(progress: number) {
-        const chache: Matrix|undefined = this._getChache();
+        const chache = this._getChache();
         if (chache && chache != this._matrix)
             this._matrix = Matrix.copy(this._matrix, chache);
-
-        if (this._updateX        && this._x       ) this._matrix.x        = this._x       .update(progress);
-        if (this._updateY        && this._y       ) this._matrix.y        = this._y       .update(progress);
-        if (this._updateScaleX   && this._scaleX  ) this._matrix.scaleX   = this._scaleX  .update(progress);
-        if (this._updateScaleY   && this._scaleY  ) this._matrix.scaleY   = this._scaleY  .update(progress);
-        if (this._updateSkewX    && this._skewX   ) this._matrix.skewX    = this._skewX   .update(progress);
-        if (this._updateSkewY    && this._skewY   ) this._matrix.skewY    = this._skewY   .update(progress);
-        if (this._updateRotation && this._rotation) this._matrix.rotation = this._rotation.update(progress);
+        
+        if (this._x       ?.isActive) this._matrix.x        = this._x       .update(progress);
+        if (this._y       ?.isActive) this._matrix.y        = this._y       .update(progress);
+        if (this._scaleX  ?.isActive) this._matrix.scaleX   = this._scaleX  .update(progress);
+        if (this._scaleY  ?.isActive) this._matrix.scaleY   = this._scaleY  .update(progress);
+        if (this._skewX   ?.isActive) this._matrix.skewX    = this._skewX   .update(progress);
+        if (this._skewY   ?.isActive) this._matrix.skewY    = this._skewY   .update(progress);
+        if (this._rotation?.isActive) this._matrix.rotation = this._rotation.update(progress);
 
         this._matrix.updateMatrix();
-        this._style?.setProperty("transform", this._matrix.toString());
+        this._style.setProperty("transform", this._matrix.toString());
         this._setChache(this._matrix);
         
         if (progress == 1) this.complete();
@@ -232,14 +209,14 @@ export class TransformUpdater implements Updater {
     }
 
     overwrite(updater: TransformUpdater) {
-        if (this._target == updater._target) {
-            if (updater._updateX       ) this._updateX        = false;
-            if (updater._updateY       ) this._updateY        = false;
-            if (updater._updateScaleX  ) this._updateScaleX   = false;
-            if (updater._updateScaleY  ) this._updateScaleY   = false;
-            if (updater._updateSkewX   ) this._updateSkewX    = false;
-            if (updater._updateSkewY   ) this._updateSkewY    = false;
-            if (updater._updateRotation) this._updateRotation = false;
+        if (this._target == updater._target && this._pseudo == updater._pseudo) {
+            if (updater._x       ) this._x       ?.unActive();
+            if (updater._y       ) this._y       ?.unActive();
+            if (updater._scaleX  ) this._scaleX  ?.unActive();
+            if (updater._scaleY  ) this._scaleY  ?.unActive();
+            if (updater._skewX   ) this._skewX   ?.unActive();
+            if (updater._skewY   ) this._skewY   ?.unActive();
+            if (updater._rotation) this._rotation?.unActive();
         }
     }
 
@@ -267,7 +244,7 @@ export class TransformUpdater implements Updater {
     }
 
     clone(target:HTMLElement = this._target, query:string|null = this._query):TransformUpdater {
-        const copy:TransformUpdater = new TransformUpdater(target, query);
+        const copy = new TransformUpdater(target, query);
 
         if (this._percentX) copy.addPropStr("x", this._percentX);
         else if   (this._x) copy._x = this._x.clone();
@@ -284,8 +261,8 @@ export class TransformUpdater implements Updater {
 
     toString():string {
         let str:string = "";
-        if (this._x       ) str += this._x       .toString() + " ";
-        if (this._y       ) str += this._y       .toString() + " ";
+        if (this._x) str += this._x.toString() + " ";
+        if (this._y) str += this._y.toString() + " ";
 
         const bx = this._x?.bezier;
         const by = this._y?.bezier;
